c0e08ca84f64b8250d2e0d2d77b355f44c1ef6e7
alexis-moins <sinus.nacre0v@icloud.com>
2023-11-20 15:18:41 +0100

    feat(back): add typescript aliases

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,15 +1,18 @@
 import express from "express";
 
+import auth from "@controllers/auth";
+
+const PORT = 3000;
 const app = express();
 
 app.use(express.json());
 
-app.get("/status", (req, res) => {
+app.get("/status", (_, res) => {
 	res.send({ healthy: true });
 });
 
-const PORT = 3000;
+app.use("/api/auth/", auth);
 
-const server = app.listen(PORT, () =>
+app.listen(PORT, () =>
 	console.log(`🚀 Server ready at: http://localhost:${PORT}`),
 );
