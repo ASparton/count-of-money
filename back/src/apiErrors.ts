3dd298c91fb7a12b01a98d6ed322fc1f19cba2e1
= <=>
2023-11-20 14:33:07 +0100

    feat(biome): biome set up

--- back/src/apiErrors.ts
+++ back/src/apiErrors.ts
@@ -2,11 +2,11 @@ import APIError from "./types/APIError";
 import HttpStatusCode from "./types/HttpStatusCode";
 
 export const ResourceNotFound: APIError = {
-  id: "RESOURCE_NOT_FOUND",
-  code: HttpStatusCode.NOT_FOUND_404,  
-}
+	id: "RESOURCE_NOT_FOUND",
+	code: HttpStatusCode.NOT_FOUND_404,
+};
 
 export const InvalidCredentials: APIError = {
-  id: "INVALID_CREDENTIALS",
-  code: HttpStatusCode.BAD_REQUEST_400
-}
+	id: "INVALID_CREDENTIALS",
+	code: HttpStatusCode.BAD_REQUEST_400,
+};
