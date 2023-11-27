70831c63fd5613ac5871a2dd9131408ea3d289d5
alexis-moins <sinus.nacre0v@icloud.com>
2023-11-27 14:29:05 +0100

    feat(controllers): add cryptos controller

--- back/src/controllers/cryptos.ts
+++ back/src/controllers/cryptos.ts
@@ -2,18 +2,36 @@ import HttpStatusCode from '#types/HttpStatusCode';
 import UrlParamIdDTO from '#types/dto/UrlParamIdDTO';
 import CreateFeedDTO from '#types/dto/feeds/CreateFeedDTO';
 import UpdateFeedDto from '#types/dto/feeds/UpdateFeedDTO';
+
 import express from 'express';
+
 import {
 	createFeed,
 	deleteFeedById,
 	findAllFeeds,
 	updateFeedById,
-} from '../database/feeds';
+} from '@database/feeds';
 
 const controller = express.Router();
 
 controller.get('/', async (_, res) => {
 	return res.status(HttpStatusCode.OK_200).send(await findAllFeeds());
+});
+
+controller.get('/:id', async (req, res) => {
+	const urlParams = UrlParamIdDTO.parse(req.params);
+	const body = UpdateFeedDto.parse(req.body);
+	return res
+		.status(HttpStatusCode.OK_200)
+		.send(await updateFeedById(urlParams.id, body.minArticlesCount));
+});
+
+controller.get('/:id/history/:period', async (req, res) => {
+	const urlParams = UrlParamIdDTO.parse(req.params);
+	const body = UpdateFeedDto.parse(req.body);
+	return res
+		.status(HttpStatusCode.OK_200)
+		.send(await updateFeedById(urlParams.id, body.minArticlesCount));
 });
 
 controller.post('/', async (req, res) => {
@@ -23,14 +41,6 @@ controller.post('/', async (req, res) => {
 		min_articles_count: body.minArticlesCount,
 	});
 	return res.status(HttpStatusCode.CREATED_201).send(createdFeed);
-});
-
-controller.put('/:id', async (req, res) => {
-	const urlParams = UrlParamIdDTO.parse(req.params);
-	const body = UpdateFeedDto.parse(req.body);
-	return res
-		.status(HttpStatusCode.OK_200)
-		.send(await updateFeedById(urlParams.id, body.minArticlesCount));
 });
 
 controller.delete('/:id', async (req, res) => {
