e5f8ce00ad29ba2db362a8cf2f1907541ea9789c
iittooss <amaury.bourguet@epitech.eu>
2023-12-14 15:29:24 +0100

    fix(front): :sparkles: change main color

--- front/src/config/mantineConfig.ts
+++ front/src/config/mantineConfig.ts
@@ -1,21 +1,21 @@
 import { createTheme, MantineColorScheme } from "@mantine/core";
 
 const theme = createTheme({
-	primaryColor: "blue",
-	colors: {
-		gray: [
-			"#f5f5f5",
-			"#e7e7e7",
-			"#cdcdcd",
-			"#b2b2b2",
-			"#9a9a9a",
-			"#8b8b8b",
-			"#848484",
-			"#717171",
-			"#656565",
-			"#575757",
-		],
-	},
+  primaryColor: "orange",
+  colors: {
+    gray: [
+      "#f5f5f5",
+      "#e7e7e7",
+      "#cdcdcd",
+      "#b2b2b2",
+      "#9a9a9a",
+      "#8b8b8b",
+      "#848484",
+      "#717171",
+      "#656565",
+      "#575757",
+    ],
+  },
 });
 
 const mantineConfig = {
